b06364e26fb5b189b02a5cd25d0a771f2c512ab3
aymenhta <aymengk94@gmail.com>
2025-08-09 11:21:52 +0100

    code tidy up

--- hc.c
+++ hc.c
@@ -16,8 +16,16 @@ void panic(const char* err)
 
 bool check_file_exists(const char* file_path)
 {
-  if (file_path == NULL) return false;
-  if (access(file_path, F_OK) == 0) return true;
+  if (file_path == NULL)
+  {
+    return false;
+  }
+  
+  if (access(file_path, F_OK) == 0)
+  {
+    return true;
+  }
+  
   return false;
 } 
 
@@ -25,12 +33,18 @@ bool check_file_exists(const char* file_path)
 void handle_file(const char* file_path)
 {
   // check if file exists
-  if (!check_file_exists(file_path)) panic("file doesn't exist!!");
-
+  if (!check_file_exists(file_path))
+  {
+    panic("file doesn't exist!!");
+  }
+  
   // open file
   FILE *f;
   f = fopen(file_path, "rb");
-  if (f == NULL) panic("couldn't open file");
+  if (f == NULL)
+  {
+    panic("couldn't open file");
+  }
 
   // print content to stdout
   int c = fgetc(f);
@@ -40,8 +54,11 @@ void handle_file(const char* file_path)
     c = fgetc(f);
   }
 
-  if (ferror(f) != 0) panic("error occured while reading file...");  
-
+  if (ferror(f) != 0)
+  {
+    panic("error occured while reading file...");  
+  }
+  
   // close file
   fclose(f);
 }
@@ -72,16 +89,20 @@ void print_usage()
 // TODO: add support for pattern matching
 int main(int argc, char **argv)
 {
-  if (argc == 1) handle_stdin();
+  if (argc == 1)
+  {
+    handle_stdin();
+  }
+  
   if (argc == 2 && (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0))
   {
       print_usage();
   }
 
-  // loop through files and display their content to stdout
   for (int i = 1; i < argc; i++)
   {
-    if (strcmp(argv[i], "-") == 0) {
+    if (strcmp(argv[i], "-") == 0)
+    {
       handle_stdin();
       continue;
     }
